2d4527c6b0be2d2a3837dbe326fcb20f6f134c02
Matan <93628116+matzco@users.noreply.github.com>
2022-04-25 14:11:28 +1000

    Create ymirdb.h

---
+++ ymirdb/ymirdb.h
@@ -0,0 +1,134 @@
+#ifndef YMIRDB_H
+#define YMIRDB_H
+
+#define MAX_KEY 16
+#define MAX_LINE 1024
+
+#include <stddef.h>
+#include <sys/types.h>
+
+enum item_type {
+    INTEGER=0,
+    ENTRY=1
+};
+
+typedef struct element element;
+typedef struct entry entry;
+typedef struct snapshot snapshot;
+
+struct element {
+  enum item_type type;
+  element* next;
+  element* prev;
+  union {
+    int value;
+    struct entry *entry;
+  };
+};
+
+struct entry {
+  char key[MAX_KEY];
+  char is_simple;
+  element * values;
+  size_t length;
+
+  entry* next;
+  entry* prev;
+  
+  size_t forward_size; 
+  size_t forward_max; 
+  entry** forward;  // this entry depends on these
+    
+  size_t backward_size; 
+  size_t backward_max; 
+  entry** backward; // these entries depend on this
+};
+
+struct snapshot {
+  int id;
+  entry* entries;
+  snapshot* next;
+  snapshot* prev;
+};
+
+void command_bye();
+
+void command_help();
+
+int free_entries(struct entry *entry);
+
+int free_elements(struct element *elem);
+
+void add_entry(char new_key[], struct entry *head_head);
+
+void add_value(struct entry *cur_entry, char new_value[]);
+
+int entries_contain(struct entry *head_head, char entry_key[]);
+
+void print_entry_values(struct entry *head_head);
+
+void print_entries(struct entry *head_head, bool only_keys);
+
+struct entry * get_entry(struct entry *head_head, char entry_key[]);
+
+void del_entry(struct entry *cur_entry);
+
+int pop_element(struct entry *cur_entry);
+
+void get_key(char key[]);
+
+int get_element_index(struct entry *cur_entry, int index, bool pluck);
+
+int number_of_elements(struct entry *cur_entry);
+
+void push_element(struct entry *cur_entry, int new_value);
+
+void append_element (struct entry *cur_entry, int new_value);
+
+int sum_elements (struct entry *cur_entry);
+
+int max_element(struct entry *cur_entry);
+
+int min_element(struct entry *cur_entry);
+
+
+
+const char* HELP =
+	"BYE   clear database and exit\n"
+	"HELP  display this help message\n"
+	"\n"
+	"LIST KEYS       displays all keys in current state\n"
+	"LIST ENTRIES    displays all entries in current state\n"
+	"LIST SNAPSHOTS  displays all snapshots in the database\n"
+	"\n"
+	"GET <key>    displays entry values\n"
+	"DEL <key>    deletes entry from current state\n"
+	"PURGE <key>  deletes entry from current state and snapshots\n"
+	"\n"
+	"SET <key> <value ...>     sets entry values\n"
+	"PUSH <key> <value ...>    pushes values to the front\n"
+	"APPEND <key> <value ...>  appends values to the back\n"
+	"\n"
+	"PICK <key> <index>   displays value at index\n"
+	"PLUCK <key> <index>  displays and removes value at index\n"
+	"POP <key>            displays and removes the front value\n"
+	"\n"
+	"DROP <id>      deletes snapshot\n"
+	"ROLLBACK <id>  restores to snapshot and deletes newer snapshots\n"
+	"CHECKOUT <id>  replaces current state with a copy of snapshot\n"
+	"SNAPSHOT       saves the current state as a snapshot\n"
+	"\n"
+	"MIN <key>  displays minimum value\n"
+	"MAX <key>  displays maximum value\n"
+	"SUM <key>  displays sum of values\n"
+	"LEN <key>  displays number of values\n"
+	"\n"
+	"REV <key>   reverses order of values (simple entry only)\n"
+	"UNIQ <key>  removes repeated adjacent values (simple entry only)\n"
+	"SORT <key>  sorts values in ascending order (simple entry only)\n"
+	"\n"
+	"FORWARD <key> lists all the forward references of this key\n"
+	"BACKWARD <key> lists all the backward references of this key\n"
+	"TYPE <key> displays if the entry of this key is simple or general\n";
+
+#endif
