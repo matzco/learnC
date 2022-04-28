3d7ec15b4ba22c466290263d7c34842115400cba
Matan <93628116+matzco@users.noreply.github.com>
2022-04-28 13:43:07 +1000

    Update lecs.c

--- lecs.c
+++ lecs.c
@@ -2,13 +2,47 @@
 #include <string.h>
 #include <stdbool.h>
 #include <stdlib.h>
+#include <limits.h>//for stuff like INT_MIN
+#include <signal.h>//signals
+#include <sys/types.h>
+#include <errno.h>//errno: extern int errno;
+#include <unistd.h>//for processes and system stuff, pipes
+#include <wait.h>//wait fork
+#include <sys/select.h>//select
+
+#define hundred 100
+//you can redefine a value, throws a warning
+
+#define min(a,b) ((a < b) ? (a) : (b) )
+#define square(a) (a*a)
+
+//JOHNS RECOMMENDED DEBUGGING STUFF, LEC 6 149
+//add compile flag -DEBUG (-D(var for define))
+#ifdef EBUG
+	#define DEBUG(s) (printf("debug: %s\n", s))
+#else
+	#define DEBUG(s) //null
+#endif
 
 //COMPILING
 //gcc -o outputFilename inputFile.c
 //can also forgo the -o flag and outputFilename, will then be a.out
 //run with ./outputFilename
 
-#define INT_MIN
+int sum(int a, int b) {
+	return a + b;
+}
+
+int product(int a, int b) {
+	return a * b;
+}
+
+struct node {
+	int value;
+	struct node *next;
+};
+
+
 
 //must have a main func
 //doesn't need to be int, can be void etc, but must be called main
@@ -171,8 +205,7 @@ int main (int argc, char **argv)
 
 	if (lec_n == 3) {
 //LEC 3
-//john drinks hot water, he prefers to tea or coffee or cold water
-//wtf
+//john drinks hot water, he prefers to tea or coffee or cold water.lol
 
 	//enums
 	//same as in other langs
@@ -331,7 +364,6 @@ int main (int argc, char **argv)
 	//flush
 	//stop everything at this point until all operations have been completed
 	//fflush
-
 	}
 
 //LEC 4
@@ -475,9 +507,7 @@ if (lec_n == 4) {
 	n2->next = NULL;
 
 	printf("reaching from head to get data in 2nd nodein LL: %d\n", head->next->data);
-
 	
-
 	struct node *n3 = (struct node*)malloc(sizeof(struct node));
 	n2->next = n3;
 	n3->data = 33;
@@ -502,18 +532,367 @@ if (lec_n == 4) {
 	free(n3);
 	free(n5);
 } 
-else if (lecs == 5) {
+
+else if (lec_n == 5) {
+	//function pointers, signals, low level I/O
+	
+	//imagine if c compiled into a high level language/version of assembly.
+	//if in c == cmpl in asm. and jmp to skip to a specific line, if the if wasn't fulfilled
+	//when we call a function, we are essentially doing a jump
+	//cos we go down program instructions, and then jump to another section of code to do something else
+	//in asm call functionname
+
+	//we can make an address pointer to a function
+	//return_type (*function_name)(param declaration);
+	int (*func_pointer)() = sum;
+	printf("using a function pointer for 3+3: %d\n", func_pointer(3,3));
+	func_pointer = product;
+	printf("reassigned function pointer, now 3*3 : %d\n", func_pointer(3,3));
+	//now we can use *func_pointer to change what function it is referencing
+	//call func_pointer with: func_pointer();
+	//in asm: call *r18 == call dereference(value in register 18)
+	
+	//can practical for basically calling a func after an if
+	//if param set function pointer to this, else to that. then call function pointer
+	//OR eg function to process, user can flag simple or complex, process_pointer = complex;
+	//use: AI in a game, can level be changed during play? if yes, prob using function pointers
+
+	//signals - relates to architecture
+	//I/O will stop running processes, and will instead process the I/O
+	//different levels, hardware, OS, application
+	//application level = signals
+	//aka software interrupt. the current running state needs to be saved, and then we go run code elsewhere
+
+	//use signals like:
+	//signal(signal_type, void_function);
+	
+	//in bash: kill -9 <program ID>
+	//get PID from ps -A|grep a.out = 18013
+	//eg: kill -s SIGSEGV PID
+	//whole load of different signals and codes, 2 is SIGINTerrupt
+
+	//side: keyword volatile int val = 5;
+	//tells compiler not to apply optimisations at all to this
+
+	//new type: sighandler_t
+	//typedef void (*sighandler_t)(int);
+	
+	//errno = error number
+	//instead of using exceptions, c has a global error number, which is set when something errors
+	//errno is set to int based on the last failed operation, if something succeeded after, won't know
+	
+	FILE *fp = fopen("this don't exist", "r");
+	printf("errno: %d, ENOENT error no entity\n", errno);
+	//after checking with errno: reset to 0
+	errno = 0;
+	//fread and fwrite can trigger interrupts, and set errno
+
+	//low level file I/O
+	//file descriptors, 0 for stdint, 1 stdout, 2 stderror
+	//creat, open, close, read, write, ioctl (I/O control), unmask
+	//creat is creat, not create
+	
+	//error will return -1, errno will also be set
+	//then save the errno as another int, so that if anything else goes wrong, you'll still have the OG errno
+	//man open 2 has all the errors
+	
+	//sigaction() is a newer version of signal(), to catch signals from sys
+	int sigaction(int signum, const struct sigaction *act, struct sigaction *oldact);
+	//sigaction struct holds a bunch of things
+	//need to pass it sa_flags = error code to catch
+
+	//Linked Lists: "There will be a heavy emphasis on this in the examination"
+}
+
+if (lec_n == 6) {
+	printf("loads of stuff on compilation: preprocessor, assembler, linker.\n");
+	//compiling: how to we go from source code to a binary executable file
+	/* .c and .h goes into preprocessor, outputs assembly .s
+	then assembler: assembly plus stuff for linking, outputs .o object
+	then linker joins all the library files into usable formats and addresses
+	outputs .out, executable binary files
+	*/
+	//compiler uses all these, but we can trigger them individually
+		//preprocessor: gcc -E
+		//assembler: gcc -s, gcc -c
+		//linker: 
+
+	//assembler makes symbols, and linker does symbol resolution
+	//meaning you can use vars declared in separate files when you join them in the linker
+	
+	//strace ./program
+	//traces all calls and processes from running a program
+
+	//static vs dynamic linked executable
+	//stuff that is #include vs stuff that is fed into linker together
+
+	//gcc --help gives list of flags incl aforementioned
+
+	//preprocessor lines start with #, it takes and then dumps text
+	//this also means that pushing code through preprocessor will not show any errors
+	//#define will search for the name and replace it with the value everywhere in the program
+	//also means if you #define twenty 10+10, the preprocessor will output that as 10+10
+		//means that something like a = 5 * twenty will give 5*10+10
+		//so if using define expressions, use brackets
+
+	//define macro
+	//ternary operator: (boolean expression) ? output if true: output if false
+	//basically use as a mini function
+	
+
+	printf("using a macro for min value (300, 4) %d\n", min(300,4));
+
+	printf("macro multiplies %d\n", square(4));
+
+	//quite a few functions are actually macros, can check in the .h or man pages
+	//eg isupper(), getc()
+
+	//preprocess conditional exclusion
+	//allows us to exclude text if we so desire, good for debugging
+	/*
+	#ifdef checks if identifier exists. if exists, will include the next text section
+		otherwise jumps to the next #ifdef
+	#if computes an expression, will include the next text, or go to
+	#else
+	#elif
+	#undef opposite of ifdef, checks if identifier does not exist
+	#endif ends the conditional statements
+	*/
+
+	/*debug mode example, final version will have a final_version defined:
+	#ifdef final_version
+	#define LINES 100
+	#else
+	#define LINES 100000
+	*/
+	//can also do something like #if WINDOWSIZE > 600 and then actual code
+
+	//for debug prints: can define a macro for printfs. see up.
+
+	DEBUG("compiled with -D flag, input EBUG for define.");	
+
+	//predefined symbols
+	//__LINE__ is a symbol that contains the current line being run
+	//same for __FILE__, current file being accessed
+
+	//<> or ""
+	//<> are system include directories, searches a few computer paths for it
+	//"" are user include directories, default loc is pwd
+} 
+
+if (lec_n == 7) {//@Gfuz9r4
+
+	printf("\nprocesses: an abstraction of convenience\n");
+
+	/*Stack heap static code*/
+	//program binaries are in static and code, stored on disk, static
+	/*but theres another section, and all of it encompassed by the process image, which is all things needed
+	for executing*/
+	//includes the OS, kernel
+	//process is basically how a program runs, data, storage, exectuion and all of it
+	//OS is designed to run processes without clashes in memory and calculations
+
+	//system memory has 2 spaces: kernel is protected, OS functions and related, only interface to hardware
+	//user memory space is everything else, prgram binaries. accesses kernel space through system calls
+
+	//fork
+	//clones a user operation, deep copy
+	//OS needs to distinguish between them, so it gets a process identifier, which is the only difference
+	
+	//virtual address space
+	//range of user memory that OS allocates to a process. has hard limits, makes it easier to jump around
+	//layouts differ between OS, not real differences between them
 
 
+	//everything in a program starts at main, and we can add char *envp[] to the main params
+	//like char *argv[], this is an array of pointers, each pointer points at a string of an environement var
+	//eg user, display, path
+	//using environment variables, we can check where the process is running from, who is running it
+		//screen size, all sorts of external info
+	//main, argc and argv are all in the stack
+	
+	//intiate process with exec() function, various forms specifically too
+	//it doesn't create a process, just replaces the current process with target one
+	//if exec() is successful, nothing will return, if something does, something went wrong
+	//this also means that everything in code after an exec() should never actually be reached
+	//execl(), execlp(), execle()
+	//getpid() from unistd.h will return the same, as the process id is the same
 
+	/*if you want to jump to a process and then return to the original: fork*/
+	//#include <sys/types.h>, <unistd.h>
+	//fork clones a process by duplicating the calling one, and will give a new PID
+	//including program counter and everything, the state is also duplicatd
+	//the processes run in parallel, the only way we can check which is which
+
+	//fork() returns an int: 
+	/* 0 means the current process running is the child
+	-1 is error
+	a number will be the PID of the child, meaning the current running process is the parent
+	*/
+	//different OS will run parent and child in different orders
+
+	printf("pre-fork PID: %d\n", getpid());
+	int cur_process = getpid();//if use this before calling fork() can have process conditional code
+	int fork_result = fork();
+	if (fork_result > 0 || getpid() == cur_process) {//can use saved PID if using multiple forks
+		printf("pausing the parent until end of child w/ wait()\n");
+		int status;
+		int x = wait(&status);
+		if (status == 0) {
+			printf("returned 0 from child, running the parent process, PIDs match.\n\n");
+		}
+	} else if (fork_result == 0) { //return 0 means running child
+		printf("running child rn, PID %d\n", getpid());
+		printf("returning 0 from child\n");
+		return 0;
+	}
+	//can fork a fork etc, and if the child 
+	printf("btw usleep to delay by microsec. can use this to influence the order of running processes\n");
+	//sleep is for seconds
+	//if using this to disrupt processes, there will be no sychronisation, and stuff will execute random order
+	//process runs in parallel, depends on cores in cpu
+
+	//so: link the 2 concepts, first fork(), then execl() the child. 
+	//control which runs first/wait while running one
+	//can use pid_t wait(int *status) where status is the return of the child process
+
+	//obviously, be super careful when forking in a loop, can really fuck up
+	//called a fork bomb, overloads memory and cpu, but fixable by shutdown
+
+	//opendir and readdir to navigate directory paths to find other files
+}
+
+if (lec_n == 8) {//from last year
+	printf("after midsem: ");
+	/*recap: use fork then exec for running new processes.
+	also wait(), can control order or process running
+	*/
+	//signals can be used between processes
+	//they can be triggered whenever, but can also be lost if signal arrives while another is processed
+	//limited to a single int
+
+	//pipes.
+	//files: fopen, fseek, fread, fclose, all used to manage low level files, easy to use
+	//pipes are unmanaged, low level I/O, only have an int to control things
+	/*low level I/O: creat, open close read write, ioctl*/
+	
+	/*pipes are for getting data from a process, put into intermediary space, and process 2 fetches it
+	use a queue, so process 2 gets in reverse order
+	pipes are unidirectional!!! have a write end and a read end
+	
+	1. ask for pipe
+	2. kernel returns a file descriptor for read and another for write: 
+	*/
+
+	//array of 2 ints
+	int fd[2] = {-1, -1};
+	
+	int ret = pipe(fd);//pipe param is address of array[2]
+	printf("called pipe(), pipes using return vals %d for read, %d for write\n", fd[0], fd[1]);
+	//should be 3 and 4, bc 0, 1, 2 are already file descriptors for stdin, stdout and error
+
+	//write to pipe: write(fd[write], string/char*, len + null byte)
+	char *msg = "hazek ahi tithazek";
+	write(fd[1], msg, (strlen(msg) + 1) );
+
+	//read from pipe
+	char buffer[5];
+	int read_error = read(fd[0], buffer, 5);
+	if (read_error <= 0) {
+		perror("couldn't read from pipe\n");
+	} else {
+		//because we aren't reading the whole string, must make the last char null byte
+		buffer[5] = '\0';
+		printf("read 5 chars from pipe: %s\n", buffer);
+	}
+
+	//the kernel orders the data
+	read_error = read(fd[0], buffer, 5);
+	if (read_error <= 0) {
+		perror("couldn't read from pipe\n");
+	} else {
+		//because we aren't reading the whole string, must make the last char null byte
+		buffer[5] = '\0';
+		printf("read 5 more chars from pipe: %s\n", buffer);
+	}
+
+	//should ofc
+	close(fd[0]);
+	close(fd[1]);
+	printf("closed the pipes but still the vals %d and %d\n", fd[0], fd[1]);
+
+	/*kernel doesn't have inf space, but more flexible than a signal, and won't have problem of lost signal
+	kernel storage is a circular buffer, an array w 2 cursors
+	1 for where was last write, 1 for last read*/
+	//john thinks of names for a pipe demo file, decides party_pipe sounds bad lol
+
+	/*comms between processes need to know the file descriptors for each pipe read/write, and also ideally 
+	can have a pipe to each other
+	create 2 pipes
+	int pid = fork()
+	if (pid != 0) we're in parent
+	else we in child
+	parent write to pipe
+	child has a read(), which is a blocking op: it will just wait for data in pipe
+		so that parent will continue running
+	note that when running parent, can close child write and parent read pipe ends, viceversa in child
+	*/
+
+	//when creating a pipe, make sure the pipe exists before forking if you want parent to access
+	//esp if a child forks again, grandparent won't be able to use the child-gchild pipe
+
+	//select() - monitors file descriptors for events
+	//read, write are synchronous, they are blocking
+	//needs a set of target file descriptors
+	//SELECT WILL NOT WAIT THE FULL DELAY IF IT CATCHES AN EVENT, WILL TRIGGER IMMEDIATELY
+
+	//init set of file descriptors, type fd_set: just a bunch of bits where index = fdnum
+	fd_set allfds;
+
+	//resets the entire set/
+	FD_ZERO(&allfds);
+
+	//adds the 1st param fd to the set
+	FD_SET(0, &allfds);
+	FD_SET(2, &allfds);
+
+	//rm an fd from fd_set type
+	FD_CLR(2, &allfds);
+
+	//max_fd should be set to the largest fd in the fd_set + 1
+	int max_fd = 0 + 1;
+	
+	//timeval struct, contains 2 long type fields for second and microsec
+	struct timeval timeout;
+	
+	//set time fields
+	timeout.tv_sec = 2;
+	timeout.tv_usec = 0;
+
+	//select(int nfds, fd_set *readfds, fd_set *writefds, fd_set *exceptfds, struct timeval *timeout);
+	int select_return = select(max_fd, &allfds, NULL, NULL, &timeout);
+
+	if (select_return == -1) {
+		fprintf(stderr, "select() errored.\n");
+	} else if (select_return == 0) {//0 == no event
+		printf("select() waited 2s, found no input for stdin.\n");
+	} else {//returns the number of fds that had events
+		printf("select() caught fd event before timeout.\n");
+		int is_changed = FD_ISSET(0, &allfds);
+		printf("event was on fd: %d\n", is_changed);
+		//could put all lines from setting timeouts into a while(1) read, strcmp
+	}
+
+	if (1)
+		;//valid code btw
+
+	//similarly
+	msg = "printf is for bitches\n";
+	write(1, msg, strlen(msg)+1 );
+} else if (lec_n == 9) {
 
 	
 }
-
-
-
-
-
+	
 	return 0;
 }
-
